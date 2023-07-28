7f2577620dfcdba13161ed2e70afb74c2f47113b
Ojolo Anuoluwapo Ifeoluwa <125829633+Phoeb2@users.noreply.github.com>
2023-07-28 14:38:04 +0100

    Update string.c

--- string.c
+++ string.c
@@ -1,11 +1,11 @@
 #include "shell.h"
 
 /**
- * _strlen - Returns length of string
+ * _strlen - It returns the length of string
  * @s: String length to check
  *
- * Return: Int length of string
- */
+ * Return: The length of string
+ **/
 
 int _strlen(char *s)
 {
@@ -20,12 +20,12 @@ int _strlen(char *s)
 }
 
 /**
- * _strcmp - Performs comparison of two strings.
+ * _strcmp - It performs the comparison of two strings.
  * @s1: The first string
  * @s2: The second string
  *
  * Return: Negative if s1 < s2, positive if s1 > s2, zero if s1 == s2
- */
+ **/
 
 int _strcmp(char *s1, char *s2)
 {
@@ -43,12 +43,12 @@ int _strcmp(char *s1, char *s2)
 }
 
 /**
- * starts_with - Checks if needle starts with haystack
+ * starts_with - It checks if the needle starts with haystack
  * @haystack: The string to search
  * @needle: The substring to find
  *
  * Return: Address of next character of haystack or NULL
- */
+ **/
 
 char *starts_with(const char *haystack, const char *needle)
 {
@@ -59,12 +59,14 @@ char *starts_with(const char *haystack, const char *needle)
 }
 
 /**
- * _strcat - Concatenates two strings
+ * _strcat - It concatenates two strings
  * @dest: Destination buffer
  * @src: Source buffer
  *
  * Return: Pointer to destination buffer
- */
+ **/
+
+
 char *_strcat(char *dest, char *src)
 {
 	char *ret = dest;
